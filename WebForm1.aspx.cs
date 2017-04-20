48a3825146036199793a31d8a7aa01f0bc75a363
sumahakkandi <sumalatha97121@gmail.com>
2017-04-20 18:19:29 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -5,21 +5,27 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace WebApplication3
+namespace management_demo
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+            if (ViewState["clicks"] != null)
+            {
+                ViewState["clicks"] = (int)ViewState["clicks"] + 1;
+            }
+            else
+            {
+                ViewState["clicks"] = 1;
+            }
+            Label1.Text = String.Format($"ViewState.clicks:{((int)ViewState["clicks"])}");
+
         }
 
-        protected void btnSubmitForm_Click(object sender, EventArgs e)
+        protected void Button1_Click(object sender, EventArgs e)
         {
-            if (Page.IsValid)
-            {
-                btnSubmitForm.Text = "My form is valid!";
-            }
+
         }
     }
 }
