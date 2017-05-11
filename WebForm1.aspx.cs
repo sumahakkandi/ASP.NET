543d235d02f0060e531254cc101e96caed08e566
sumahakkandi <sumalatha97121@gmail.com>
2017-05-11 10:48:10 +0530

    Add files via upload

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,31 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace management_demo
+namespace Suma
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (ViewState["clicks"] != null)
-            {
-                ViewState["clicks"] = (int)ViewState["clicks"] + 1;
-            }
-            else
-            {
-                ViewState["clicks"] = 1;
-            }
-            Label1.Text = String.Format($"ViewState.clicks:{((int)ViewState["clicks"])}");
-
-        }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
+            Thread.Sleep(1000);
         }
     }
 }
